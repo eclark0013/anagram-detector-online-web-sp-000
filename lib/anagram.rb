414b1b1bb3a7fd1a6cf50d689ee0d245538dfbc7
Eric Clark <eclark0013@gmail.com>
2019-06-26 13:13:21 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,13 @@
 # Your code goes here!
 class Anagram
+  attr_accessor :word, :check_array
+
+  def initialize(word)
+    @word=word
+  end
+
+  def match(string)
+    @check_array=string.split(" ")
+    @check_array.collect do |checker|
+      checker.split("").sort==word.split("").sort
 end
